f34cef96104619e3c780da5fa82591c7b8b7ede8
rony_sartor@hotmail.com <rony_sartor@hotmail.com@75320e76-1a6b-11de-9308-ab3afd9c0aba>
2009-04-18 23:16:32 +0000

    Inclusão de duas internal frames das aplicacao

--- trunk/LOGIN_CONTROLER/src/com/pucpr/br/frontend/utils/ConstantsFrontEnd.java
+++ trunk/LOGIN_CONTROLER/src/com/pucpr/br/frontend/utils/ConstantsFrontEnd.java
@@ -3,8 +3,7 @@ package com.pucpr.br.frontend.utils;
 public class ConstantsFrontEnd {
 	
 	public static final String LOGIN_CONFIRMAR = "Confirmar";
-	public static final String LOGIN_NOVO_USUARIO = "Novo usuario";
-	public static final String LOGIN_CANCELAR = "Cancelar";
+	public static final String LOGIN_NOVO_USUARIO = "Novo usuário";	
 	public static final String LOGIN_LOGIN = "Login :";
 	public static final String LOGIN_SENHA = "Senha :";
 	public static final String LOGIN_TITULO_TELA_LOGIN =":: LOGIN - PROJETO INTEGRADO ::";
@@ -14,7 +13,7 @@ public class ConstantsFrontEnd {
 	public static final String USUARIO_SENHA = "Senha :";
 	public static final String USUARIO_CONFIRMAR_SENHA = "Confirmar Senha :";	
 	public static final String USUARIO_CANCELAR = "Cancelar";
-	public static final String USUARIO_SALVAR = "Salvar";
+	public static final String USUARIO_CONFIRMAR = "Confirmar";
 	public static final String USUARIO_TITULO_TELA_NOVO_USUARIO = ":: NOVO USUARIO - PROJETO INTEGRADO ::";
 	public static final String USUARIO_TITULO_TELA_EDITAR_USUARIO = ":: EDITAR USUARIO - PROJETO INTEGRADO ::";
 
